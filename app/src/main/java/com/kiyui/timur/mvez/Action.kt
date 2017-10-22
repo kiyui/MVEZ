da7999b0ab12c8f12286715ae1e91797dcbfd151
Timur Kiyui <timur@linux.com>
2017-10-22 10:50:42 +0800

    feat(app): class to encapsulate intents

---
+++ app/src/main/java/com/kiyui/timur/mvez/Action.kt
@@ -0,0 +1,11 @@
+package com.kiyui.timur.mvez
+
+/**
+ * All intents are in the form of an `Action`
+ * where they are named and may carry a `value`
+ * for the subscription to perform side-effects
+ */
+class Action(name: String, value: Any) {
+    val name = name
+    val value = value
+}
